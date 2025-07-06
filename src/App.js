6f113528d04921c374b9a54e570016d6d4d606b6
Iza-04 <Iza.m150492@gmail.com>
2025-07-06 15:22:14 +0500

    add ternary operator, add components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -16,18 +22,37 @@ function Logo() {
 function Form() {
   return (
     <div className="add-form">
-      <h3>What do you needr your trip?🌴</h3>
+      <h3>What do you need for your trip?🌴</h3>
     </div>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
