d3de0ab526ae2ffde2582ac63257bc8d1159fd8b
Iza-04 <Iza.m150492@gmail.com>
2025-07-23 15:58:10 +0500

    add description

--- src/App.js
+++ src/App.js
@@ -23,17 +23,26 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if(!description) return;
+
+    const newItem = {description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+      setQuantity(1);
+
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?🌴</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select value={quantity} onChange={(e) => setQuantity
+      (Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
